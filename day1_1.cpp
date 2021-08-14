3a0795abe15921434865cd54805662a96eadd89b
Rohitkumarvarma-369 <79484529+Rohitkumarvarma-369@users.noreply.github.com>
2021-08-14 15:12:27 +0530

    Update day1_1.cpp

--- day1_1.cpp
+++ day1_1.cpp
@@ -3,138 +3,103 @@
 Rohit Kumar Varma (@immortal_1 at codechef and codeforces)
 Amrita Vishwa Vidyapeetham, Amritapuri
 
-//Dutch National Flag alogirthm (similar)
-QN: Sort an array of 0’s 1’s 2’s without using extra space or sorting algo LINK: https://leetcode.com/problems/sort-colors/
+Title: Arrays: Sort an array of 0’s 1’s 2’s without using extra space or sorting algo 
 
-Bruteforce: Just sort : Time complexity: O(nlogn), Space complexity: O(1);
-Better: Count sort: Time complexity: O(2n)
-Optimal: Dutch national falf algorithm (low-mid-high): Time complexity: O(n), Space complexity: O(1)
+Overview: https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=2
+
+Brute: just sort the array and print it T.C: O(nlogn), S.C: O(1);
+
+Better:  count sort T.C: O(N), S.C: O(N);
+
+Optimal: dutch national flag algorithm T.C: O(N), S.C: O(1);
+
+Extra notes: 
 
 */
 
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-void bruteforcesoln(int arr[], int n){
-
-	sort(arr,arr+n);
-
-	cout<<"Bruteforce solution: ";
-
+//Utilities functions
+void printarray(int arr[], int n){
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
 	cout<<endl;
 }
 
-void bettersoln(int arr[], int n){
-	int countzeroes, countones, counttwos;
-	countzeroes = 0;
-	countones = 0;
-	counttwos = 0;
+void brute(int arr[], int n){
+	sort(arr, arr+n);
+	printarray(arr,n);
+}
+
+void better(int arr[], int n){
+
+	int countarr[3] = {0};
 
 	for(int i=0;i<n;i++){
 		if(arr[i]==0){
-			++countzeroes;
+			++countarr[0];
 		}
 		else if(arr[i]==1){
-			++countones;
+			++countarr[1];
 		}
 		else{
-			++counttwos;
+			++countarr[2];
 		}
 	}
-
-	int ansarr[n];
-
-	for(int i=0;i<n;i++){
-		if(countzeroes!=0){
-			ansarr[i] = 0;
-			--countzeroes;
-		}
-		else if(countones!=0){
-			ansarr[i] = 1;
-			--countones;
-		}
-		else{
-
-			ansarr[i] = 2;
-			--counttwos;
-
+	int curr = 0;
+	for(int i=0;i<3;i++){
+		int val = countarr[i];
+		int tempcurr = curr;
+		for(int j = tempcurr; j<val;j++){
+			arr[j] = i;
+			++curr;
 		}
 	}
+	printarray(arr,n);
 
-	cout<<"Better solution: ";
-
-	for(int i=0;i<n;i++){
-		cout<<ansarr[i]<<" ";
-	}
-	cout<<endl;
 }
 
-void bestsoln(int arr[], int n){
-	int low = 0;
-	int high = n-1;
-	int mid = 0;
+void optimal(int arr[], int n){
+	int low, mid, high;
+	low = mid = 0;
+	high = n-1;
 
-	while(mid<=high){//condition till which the algo continues
-		if(arr[mid]==0){//check when 0
-
-			int temp1 = arr[low];
-			arr[low] = arr[mid];
-			arr[mid] = temp1;
+	for(int i = 0;i<n;i++){
+		if(arr[mid]==1){
+			++mid;
+			continue;
+		}
+		else if(arr[mid]==0){
+			swap(arr[mid], arr[low]);
 			++low;
 			++mid;
-			continue;
-
 		}
-		else if(arr[mid]==1){//check when 1
-
-			++mid;
-			continue;
-
-		}
-		else{//check when 2
-			int temp2 = arr[high];
-			arr[high] = arr[mid];
-			arr[mid] = temp2;
+		else{
+			swap(arr[mid], arr[high]);
 			--high;
-			continue;
-
 		}
 	}
-	cout<<"Most Optimised solution: ";
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
 
+	printarray(arr,n);
 }
+
 
 
 int main(){
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-
-    	cin>>arr[i];
-
-    }
-
-
-    //bruteforce solution
-    bruteforcesoln(arr,n);
-
-    //better solution
-    bettersoln(arr, n);
-
-    //most optimised soln
-    //Dutch National Flag algorithm
-    bestsoln(arr,n);
-
 	
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	brute(arr,n);//sort
+	better(arr,n);//count sort
+	optimal(arr,n);//dutch national flag algorithm
+
 }
